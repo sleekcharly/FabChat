3f614763f23d3e746786cc22787269165bfb451f
Charles <c_ukasoanya@yahoo.com>
2023-11-04 01:19:29 +0100

    created Picing page

---
+++ components/PricingCards.tsx
@@ -0,0 +1,30 @@
+// array entries
+const tiers = [
+  {
+    name: 'Starter',
+    id: null,
+    href: '#',
+    priceMonthly: null,
+    description: 'Get chatting right away with anyone, anywhere!',
+    features: [
+      '20 Message Chat limit in Chats',
+      '2 Participant limit in Chat',
+      '3 Chat Rooms Limit',
+      'Supports 2 languages',
+      '48-hour support response time',
+    ],
+  },
+  {
+    name: 'Pro',
+    id: 'sldbnskns;knklk',
+    href: '#',
+    priceMonthly: 'N1500',
+    description: 'Unlock',
+  },
+];
+
+function PricingCards({ redirect }: { redirect: boolean }) {
+  return <div>PricingCards</div>;
+}
+
+export default PricingCards;
